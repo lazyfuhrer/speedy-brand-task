20b2d2ec1a84526f4028a94477f4ba1866814a3c
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-06-14 18:41:01 +0530

    fix: shited tabsdata to seperate file

---
+++ src/constant/TabsData.js
@@ -0,0 +1,90 @@
+export const TabsData = [
+    // Tabs, Topics and Tags data
+    {
+      name: 'All',
+      topics: [
+        {
+          name: 'The importance of establishing a Strong Online Presence for Small Businesses',
+          tags: ['online presence', 'small businesses', 'digital marketing', 'branding'],
+        },
+        {
+          name: 'How fast Turnaround Times in logo and website design can help your business',
+          tags: ['fast turnaround', 'logo design', 'website design', 'branding'],
+        },
+        {
+          name: 'Affordable branding solutions for startups and entrepreneurs',
+          tags: ['affordable branding', 'startups', 'entrepreneurs', 'logo design', 'website design'],
+        },
+        {
+          name: 'The Benifits of Compreshensive Branding services for Small to Medium-sized Businesses',
+          tags: ['compreshensive Branding', 'small businesses', 'logo design', 'website design', 'social media engagement'],
+        },
+        {
+          name: 'Expert tips for choosing the right digital marketing agency for your business',
+          tags: ['digital marketing agency', 'tips','branding', 'website design', 'social media engagement'],
+        },
+      ],
+    },
+    {
+      name: 'Custom',
+      topics: [
+        {
+          name: 'The importance of establishing a Strong Online Presence for Small Businesses',
+          tags: ['online presence', 'small businesses', 'digital marketing', 'branding'],
+        },
+      ],
+    },
+    // Add more tabs here
+    {
+      name: 'ICP',
+      topics: [
+        {
+          name: 'The importance of establishing a Strong Online Presence for Small Businesses',
+          tags: ['online presence', 'small businesses', 'digital marketing', 'branding'],
+        },
+        {
+          name: 'How fast Turnaround Times in logo and website design can help your business',
+          tags: ['fast turnaround', 'logo design', 'website design', 'branding'],
+        },
+      ],
+    },
+    {
+      name: 'Mission',
+      topics: [
+        {
+          name: 'The importance of establishing a Strong Online Presence for Small Businesses',
+          tags: ['online presence', 'small businesses', 'digital marketing', 'branding'],
+        },
+        {
+          name: 'How fast Turnaround Times in logo and website design can help your business',
+          tags: ['fast turnaround', 'logo design', 'website design', 'branding'],
+        },
+        {
+          name: 'Affordable branding solutions for startups and entrepreneurs',
+          tags: ['affordable branding', 'startups', 'entrepreneurs', 'logo design', 'website design'],
+        },
+      ],
+    },
+    {
+      name: 'Product',
+      topics: [
+        {
+          name: 'The importance of establishing a Strong Online Presence for Small Businesses',
+          tags: ['online presence', 'small businesses', 'digital marketing', 'branding'],
+        },
+        {
+          name: 'How fast Turnaround Times in logo and website design can help your business',
+          tags: ['fast turnaround', 'logo design', 'website design', 'branding'],
+        },
+        {
+          name: 'Affordable branding solutions for startups and entrepreneurs',
+          tags: ['affordable branding', 'startups', 'entrepreneurs', 'logo design', 'website design'],
+        },
+        {
+          name: 'The Benifits of Compreshensive Branding services for Small to Medium-sized Businesses',
+          tags: ['compreshensive Branding', 'small businesses', 'logo design', 'website design', 'social media engagement'],
+        },
+      ],
+    },
+  ];
+  
